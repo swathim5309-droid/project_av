d8d65d9f681ca6a1ff8b8199945b530294c82b72
swathim5309-droid <swathim5309@gmail.com>
2025-12-25 18:22:01 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   projectId: "studio-9832230977-6c6ca",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_FIREBASE_API_KEY,
+  appId: process.env.NEXT_FIREBASE_APP_ID,
   authDomain: "studio-9832230977-6c6ca.firebaseapp.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // optional
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, // optional
+  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_FIREBASE_MEASUREMENT_ID, // optional
 };
 
 export const app = initializeApp(firebaseConfig);
